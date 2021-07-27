bad76b78c83018d5ded92a65381b93e2af32b79d
Matek <matiowo@wp.pl>
2021-07-27 02:15:07 +0200

    Add files via upload

--- pospolite.view.css.selector.pas
+++ pospolite.view.css.selector.pas
@@ -35,7 +35,7 @@ type
   public
     constructor Create(const AContext: TPLCSSSelectorContext); virtual;
 
-    property Context: TPLCSSSelectorContext read FContext;
+    property Context: TPLCSSSelectorContext read FContext write FContext;
   end;
 
   { TPLCSSSimpleSelector }
@@ -132,24 +132,27 @@ type
   public
     constructor Create(const AContext: TPLCSSSelectorContext; const AValue: TPLString); reintroduce;
 
-    property Value: TPLString read FValue;
+    property Value: TPLString read FValue write FValue;
   end;
 
   { TPLCSSSelectorAttribute }
 
   TPLCSSSelectorAttribute = packed class(TPLCSSSimpleSelector)
   private
+    FCaseSensitive: TPLBool;
     FComparator: TPLString;
     FName: TPLString;
     FValue: TPLCSSSelectorString;
   public
     constructor Create(const AContext: TPLCSSSelectorContext; const AName,
-      AComparator: TPLString; const AValue: TPLCSSSelectorString); reintroduce;
+      AComparator: TPLString; const AValue: TPLCSSSelectorString;
+      const ACaseSensitive: TPLBool = false); reintroduce;
     destructor Destroy; override;
 
     property Name: TPLString read FName;
     property Comparator: TPLString read FComparator;
     property Value: TPLCSSSelectorString read FValue;
+    property CaseSensitive: TPLBool read FCaseSensitive;
   end;
 
   { TPLCSSSelectorClass }
@@ -207,6 +210,8 @@ type
   end;
 
 implementation
+
+uses character, strutils, LazUTF8;
 
 { TPLCSSSelectorContext }
 
@@ -391,13 +396,14 @@ end;
 
 constructor TPLCSSSelectorAttribute.Create(
   const AContext: TPLCSSSelectorContext; const AName, AComparator: TPLString;
-  const AValue: TPLCSSSelectorString);
+  const AValue: TPLCSSSelectorString; const ACaseSensitive: TPLBool);
 begin
   inherited Create(Context);
 
   FName := AName;
   FComparator := AComparator;
   FValue := AValue;
+  FCaseSensitive := ACaseSensitive;
 end;
 
 destructor TPLCSSSelectorAttribute.Destroy;
@@ -464,14 +470,223 @@ var
     while not IsEOF and Current.IsWhiteSpace do Inc(pos);
   end;
 
+  function IsSpecialPseudo(const APseudo: TPLString): TPLBool; inline;
+  begin
+    Result := APseudo.ToLower in TPLStringFuncs.NewArray(['after', 'before',
+      'first-letter', 'first-line', 'marker', 'placeholder', 'selection']);
+  end;
+
+  function NewIdentifier: TPLString;
+  begin
+    Result := '';
+    if not IsEOF and (Current <> '_') and not TCharacter.IsLetter(Current) then exit;
+
+    while not IsEOF and (TCharacter.IsLetterOrDigit(Current) or (Current in ['_', '-'])) do begin
+      Result += Current;
+      Inc(pos);
+    end;
+  end;
+
+  function NewString: TPLCSSSelectorString;
+  var
+    q: TPLChar;
+    esc: TPLBool = false;
+    v, pom, pom2: TPLString;
+    i, p: SizeInt;
+  begin
+    q := Current;
+    v := '';
+    pom := '';
+    p := pos;
+    Inc(pos);
+
+    while not IsEOF do begin
+      if esc then begin
+        if Current in ['0'..'9', 'A'..'F', 'a'..'f'] then begin
+          pom2 := '';
+          for i := 0 to 5 do begin
+            if not (Current in ['0'..'9', 'A'..'F', 'a'..'f']) then break;
+            pom2 += Current;
+            Inc(pos);
+          end;
+          pom2 := UnicodeToUTF8(pom2.FromHex);
+          v += pom2;
+          pom += pom2;
+        end else begin
+          v += Current;
+          pom += Current;
+        end;
+        esc := false;
+      end else begin
+        if Current = q then begin
+          Inc(pos);
+          break;
+        end;
+        pom += Current;
+        if Current = '\' then esc := true else v += Current;
+      end;
+
+      Inc(pos);
+    end;
+
+    Result := TPLCSSSelectorString.Create(TPLCSSSelectorContext.Create(ASelector, pom, p, pos), v);
+  end;
+
+  function NewAttribute: TPLCSSSelectorAttribute;
+  var
+    sub: TPLString = '[';
+    pom, pom2, op: TPLString;
+    cs: TPLBool;
+    p, pp: SizeInt;
+    v: TPLCSSSelectorString = nil;
+    q: TPLChar;
+  begin
+    op := '';
+    p := pos;
+    ConsumeWhitespace;
+    pom := '';
+    if not IsEOF then pom := NewIdentifier;
+    if pom.IsEmpty then pom := 'attr';
+    sub += pom;
+    ConsumeWhitespace;
+
+    if not isEOF then begin
+      if Current in ['~', '*', '$', '|', '^'] then begin
+        op := Current + '=';
+        Inc(pos);
+        if IsEOF or (Current <> '=') then begin
+          if not IsEOF and not (Current in ['''', '"']) then Inc(pos);
+        end else Inc(pos);
+      end else if Current <> ']' then begin
+        if Current = '=' then op := '=';
+        Inc(pos);
+      end;
+
+      ConsumeWhitespace;
+      if not IsEOF then begin
+        if op <> '' then begin
+          sub += op;
+          if Current in ['''', '"'] then begin
+            q := Current;
+            v := NewString;
+            sub += q + v.Context.Value + q;
+          end else begin
+            pp := pos;
+            pom2 := NewIdentifier;
+            sub += pom2;
+            v := TPLCSSSelectorString.Create(TPLCSSSelectorContext.Create(ASelector, pom2, pp, pos), pom2);
+          end;
+        end;
+
+        ConsumeWhitespace;
+        if LowerCase(Current) in ['i', 's'] then begin
+          cs := LowerCase(Current) = 's';
+          Inc(pos);
+        end;
+        ConsumeWhitespace;
+        if not IsEOF then Inc(pos);
+      end;
+    end;
+
+    Result := TPLCSSSelectorAttribute.Create(TPLCSSSelectorContext.Create(ASelector, sub + ']', p, pos), pom, op, v, cs);
+  end;
+
+  function NewPseudo(AName: TPLString; ANamespaced: TPLBool; AKind: TPLCSSSelectorPseudoKind): TPLCSSSelectorPseudo;
+  var
+    expr: TPLCSSSelectorPseudoExpression = nil;
+    sub: TPLString = '';
+    p: SizeInt;
+  begin
+    p := pos;
+    //...
+  end;
+
   function NewPattern: TPLCSSSimpleSelectorPattern;
   var
-    p: SizeInt;
+    p, pp, sc: SizeInt;
     sub: TPLString = '';
+    ss: TPLBool = false;
+    pom: TPLString;
+    ps, dc: TPLBool;
   begin
     p := pos;
     Result := TPLCSSSimpleSelectorPattern.Create(TPLCSSSelectorContext.Create(ASelector, '', p, pos));
-    //...
+
+    if Current = '*' then begin
+      ss := true;
+      sub += Current;
+      Result.SimpleSelectors.Add(TPLCSSSelectorType.Create(TPLCSSSelectorContext.Create(ASelector, '*', p, pos), '*'));
+      Inc(pos);
+    end else if TCharacter.IsLetter(Current) then begin
+      ss := true;
+      pom := NewIdentifier;
+      sub += pom;
+      Result.SimpleSelectors.Add(TPLCSSSelectorType.Create(TPLCSSSelectorContext.Create(ASelector, pom, p, pos), pom));
+    end;
+
+    sc := 0;
+    ps := false;
+    while not IsEOF do begin
+      pp := pos;
+      case Current of
+        '.': begin
+          Inc(pos);
+          if not IsEOF then pom := NewIdentifier else pom := '';
+          if not pom.IsEmpty then begin
+            sub += '.' + pom;
+            Result.SimpleSelectors.Add(TPLCSSSelectorClass.Create(TPLCSSSelectorContext.Create(ASelector, '.' + pom, pp, pos), pom));
+          end;
+        end;
+        '#': begin
+          Inc(pos);
+          if not IsEOF then pom := NewIdentifier else pom := '';
+          if not pom.IsEmpty then begin
+            sub += '#' + pom;
+            Result.SimpleSelectors.Add(TPLCSSSelectorHash.Create(TPLCSSSelectorContext.Create(ASelector, '#' + pom, pp, pos), pom));
+          end;
+        end;
+        '[': begin
+          pp := pos;
+          Inc(pos);
+          Result.SimpleSelectors.Add(NewAttribute);
+          sub += ASelector.SubStr(pp, pos - pp + 1);
+        end;
+        ':': begin
+          dc := false;
+          Inc(pos);
+          sub += ':';
+
+          if IsEOF then break;
+          if Current = ':' then begin
+            dc := true;
+            sub += ':';
+            Inc(pos);
+          end;
+
+          if not IsEOF then pom := NewIdentifier.ToLower else pom := '';
+          if not pom.IsEmpty then begin
+            if IsSpecialPseudo(pom) or dc then begin
+              sub += pom;
+              Result.SimpleSelectors.Add(NewPseudo(pom, dc, pskElement));
+            end else begin
+              case pom of
+                'not': begin
+                  //...
+                end;
+                // zrobić :is, :where, :nth-...
+                else begin
+                  sub += pom;
+                  Result.SimpleSelectors.Add(NewPseudo(pom, dc, pskClass));
+                end;
+              end;
+            end;
+
+            Inc(sc);
+          end;
+        end;
+        else break;
+      end;
+    end;
 
     with Result.Context do begin
       Value := sub;
@@ -524,10 +739,11 @@ var
         end;
 
         Result.Combinators.Add(c);
-        p := NewPattern;
-        sub += p.Context.Value;
-        Result.SimpleSelectors.Add(p);
       end;
+
+      p := NewPattern;
+      sub += p.Context.Value;
+      Result.SimpleSelectors.Add(p);
     end;
 
     with Result.Context do begin
